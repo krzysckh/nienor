51c505cf5d4edf0c3e64f13768054dcdf0f50b86
krzysckh <krzysckh@wp.pl>
2025-06-09 09:47:24 +0200

    remove a lie from t/test-func.scm

--- t/test-func.scm
+++ t/test-func.scm
@@ -1,17 +1,15 @@
 (define (app f)
   (f #\n #\k))
 
+(define (app* f)
+  (λ (x) (f x)))
+
 (define (main)
   (app (λ (a b)
          (putchar ((λ (c) (+ c ((λ (x) x) 1))) a))
-         (putchar b)
-         (putchar #\newline)))
+         (putchar b)))
+  ((app* (λ (c) (putchar c))) (- ((fuck #\newline) 32) 32))
   (exit 128))
 
-;; We don't have closures though. sorry.
-;;
-;; (define (fuck a)
-;;   (λ (b) (+ a b)))
-;;
-;; will error out with:
-;; error: (couldn't resolve symbol  a)
+(define (fuck a)
+  (λ (b) (+ a b)))
