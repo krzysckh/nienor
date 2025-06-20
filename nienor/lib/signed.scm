d460c3b0e27556e753551a65ca120c3807ce9ac3
krzysckh <kpm@krzysckh.org>
2025-06-20 12:59:55 +0200

    move font stuff to its own file, add signed {+,-,/,*} emulation

---
+++ nienor/lib/signed.scm
@@ -0,0 +1,47 @@
+(define (_print-signed-number n depth)
+  (when (sign n)
+    (putchar #\-))
+  (if (equ? 1 (band (equ? n 0) (> depth 0)))
+      (noop)
+      (let ((n (band #x7fff n)))
+        (_print-signed-number (/ n 10) (+ depth 1))
+        (putchar (+ #\0 (modulo n 10))))))
+
+(define-macro-rule ()
+  (print-signed-number n)
+  (begin
+    (_print-signed-number n 0)
+    (putchar #\newline)))
+
+(define (signed+ a b)
+  (let ((a (if (sign a) (+ 1 (bxor #xffff (band #x7fff a))) a))
+        (b (if (sign b) (+ 1 (bxor #xffff (band #x7fff b))) b)))
+    (let ((res (+ a b)))
+      (if (sign res)
+          (+ 1 (bxor #xffff (band #x7fff res)))
+          res))))
+
+(define (signed- a b)
+  (signed+ a (bxor #x8000 b)))
+
+(define-macro-rule ()
+  (sign x)
+  (band #x8000 x))
+
+(define (signed* a b)
+  (bior
+   (bxor (sign a) (sign b))
+   (* (signed a) (signed b))))
+
+(define (signed/ a b)
+  (bior
+   (bxor (sign a) (sign b))
+   (/ (signed a) (signed b))))
+
+(define-macro-rule ()
+  (signed x)
+  (bxor #x8000 (negative x)))
+
+(define-macro-rule ()
+  (negative x)
+  (bior #x8000 x))
--- t/test-signed.scm
+++ t/test-signed.scm
@@ -1,45 +1,29 @@
 (_declare-test
- output => "-1\n1\n1\n-1\n-1\n1\n1\n-1\n-10\n")
+ output => "-1\n1\n1\n-1\n-1\n1\n1\n-1\n-10\n-6\n6\n-6\n6\n-2\n2\n-2\n2\n")
 
-(define (_print-signed-number n depth)
-  (when (band #x8000 n)
-    (putchar #\-))
-  (if (equ? 1 (band (equ? n 0) (> depth 0)))
-      (noop)
-      (let ((n (band #x7fff n)))
-        (_print-signed-number (/ n 10) (+ depth 1))
-        (putchar (+ #\0 (modulo n 10))))))
-
-(define-macro-rule ()
-  (print-signed-number n)
-  (begin
-    (_print-signed-number n 0)
-    (putchar #\newline)))
-
-(define (signed+ a b)
-  (let ((sign-a (band #x8000 a))
-        (sign-b (band #x8000 b)))
-    (let ((a (if sign-a (+ 1 (bxor #xffff (band #x7fff a))) a))
-          (b (if sign-b (+ 1 (bxor #xffff (band #x7fff b))) b)))
-      (let ((res (+ a b)))
-        (if (band #x8000 res)
-            (+ 1 (bxor #xffff (band #x7fff res)))
-            res)))))
-
-(define (signed- a b)
-  (signed+ a (bxor #x8000 b)))
+;; TODO: maybe do some compiler magic type deduction in some far far future
 
 (define (main)
-  (print-signed-number (signed+ 10 (bior #x8000 11)))
-  (print-signed-number (signed+ 10 (bior #x8000 9)))
-  (print-signed-number (signed+ (bior #x8000 10) 11))
-  (print-signed-number (signed+ (bior #x8000 10) 9))
+  (print-signed-number (signed+ 10 (negative 11)))
+  (print-signed-number (signed+ 10 (negative 9)))
+  (print-signed-number (signed+ (negative 10) 11))
+  (print-signed-number (signed+ (negative 10) 9))
 
   (print-signed-number (signed- 10 11))
   (print-signed-number (signed- 10 9))
-  (print-signed-number (signed- (bior #x8000 10) (bior #x8000 11)))
-  (print-signed-number (signed- (bior #x8000 10) (bior #x8000 9)))
+  (print-signed-number (signed- (negative 10) (negative 11)))
+  (print-signed-number (signed- (negative 10) (negative 9)))
 
   (print-signed-number (signed- 0 10))
+
+  (print-signed-number (signed* (negative 2) 3))
+  (print-signed-number (signed* (negative 2) (negative 3)))
+  (print-signed-number (signed* 2 (negative 3)))
+  (print-signed-number (signed* 2 3))
+
+  (print-signed-number (signed/ (negative 6) 3))
+  (print-signed-number (signed/ (negative 6) (negative 3)))
+  (print-signed-number (signed/ 6 (negative 3)))
+  (print-signed-number (signed/ 6 3))
 
   (exit 128))
