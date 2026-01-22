59244fc37cd9cffb02843bf2367a8048620f2af9
Krzysztof Michałczyk <kpm@krzysckh.org>
2026-01-22 15:46:30 +0100

    more _ hooks in brainfuck interpreter

--- t/test-brainfuck.scm
+++ t/test-brainfuck.scm
@@ -11,51 +11,47 @@
 
 ;; +
 (define-macro-rule (+ _ I)
-  (_bf (_ . left) (_ v . right)       (+ . code))
-  (_bf (_ . left) (_ (I . v) . right) code))
-
-(define-macro-rule (+ _ I ())
-  (_bf (_ . left) (_ () . right)  (+ . code))
-  (_bf (_ . left) (_ (I) . right) code))
+  (_bf (_ . left) (_ (_ . v) . right)   (+ . code))
+  (_bf (_ . left) (_ (_ I . v) . right) code))
 
 (define-macro-rule (+ _ I)
-  (_bf (_ . left) (_)     (+ . code))
-  (_bf (_ . left) (_ (I)) code))
+  (_bf (_ . left) (_)       (+ . code))
+  (_bf (_ . left) (_ (_ I)) code))
 
 (define-macro-rule (+ _ I)
   (_bf (_ . left)
-       (_ (I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I ; overflow
+       (_ (_ I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I ; overflow
            I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I
            I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I
            I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I
            I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I
            I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I
            I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I
-           I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I)
+           I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I)
           . right)
        (+ . code))
-  (_bf (_ . left) (_ () . right) code))
+  (_bf (_ . left) (_ (_) . right) code))
 ;; end +
 
 ;; -
 (define-macro-rule (- _)
-  (_bf (_ . left) (_ (v . rest) . right) (- . code))
-  (_bf (_ . left) (_ rest . right)       code))
+  (_bf (_ . left) (_ (_ v . rest) . right) (- . code))
+  (_bf (_ . left) (_ (_ . rest) . right)   code))
 
-(define-macro-rule (- _ ())
-  (_bf (_ . left) (_)    (- . code))
-  (_bf (_ . left) (_ ()) (- . code)))
+(define-macro-rule (- _)
+  (_bf (_ . left) (_)     (- . code))
+  (_bf (_ . left) (_ (_)) (- . code)))
 
-(define-macro-rule (- _ ())
-  (_bf (_ . left) (_ () . right) (- . code))
-  (_bf (_ . left) (_ (I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I ; underflow
+(define-macro-rule (- _)
+  (_bf (_ . left) (_ (_) . right) (- . code))
+  (_bf (_ . left) (_ (_ I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I ; underflow
                       I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I
                       I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I
                       I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I
                       I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I
                       I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I
                       I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I
-                      I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I) . right) code))
+                      I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I I) . right) code))
 ;; end -
 
 ;; >
@@ -63,23 +59,15 @@
   (_bf (_ . left)   (_ v . right) (> . code))
   (_bf (_ v . left) (_ . right)   code))
 
-(define-macro-rule (> _ ())
-  (_bf (_ . left)    (_ () . right) (> . code))
-  (_bf (_ () . left) (_ . right)    code))
-
-(define-macro-rule (> _ ())
-  (_bf (_ . left)   (_)  (> . code))
-  (_bf (_ () . left) (_) code))
+(define-macro-rule (> _)
+  (_bf (_ . left)     (_)  (> . code))
+  (_bf (_ (_) . left) (_) code))
 ;; end >
 
 ;; <
 (define-macro-rule (< _)
   (_bf (_ v . left) (_ . right)   (< . code))
   (_bf (_ . left)   (_ v . right) code))
-
-(define-macro-rule (< _ ())
-  (_bf (_ () . left) (_ . right)   (< . code))
-  (_bf (_ . left)   (_ () . right) code))
 
 (define-macro-rule (< _)
   (_bf (_) (_ . right) (< . code))
@@ -96,14 +84,14 @@
   0)
 
 (define-macro-rule (p _)
-  (_bf (_ . left) (_ c . right) (p . code))
+  (_bf (_ . left) (_ (_ . c) . right) (p . code))
   (begin
     (putchar (_cnt . c))
-    (_bf (_ . left) (_ c . right) code)))
+    (_bf (_ . left) (_ (_ . c) . right) code)))
 
 (define-macro-rule (p _)
   (_bf (_ . left) (_)   (p . code))
-  (_bf (_ . left) (_ ()) (p . code)))
+  (_bf (_ . left) (_ (_)) (p . code)))
 ;; end .
 
 (define-macro-rule (_)
@@ -127,9 +115,9 @@
   (__m_reverse . rs))
 
 ;; [
-(define-macro-rule (_ ())
-  (_bf (_ . left) (_)    ((x . xs) . code))
-  (_bf (_ . left) (_ ()) ((x . xs) . code)))
+(define-macro-rule (_)
+  (_bf (_ . left) (_)     ((x . xs) . code))
+  (_bf (_ . left) (_ (_)) ((x . xs) . code)))
 
 (define-macro-rule (_)
   (_bf (_ . left) (_ v . right) ((x . xs) . code))
@@ -138,9 +126,9 @@
                                  ((x . xs))
                                  code)))
 
-(define-macro-rule (_ ())
-  (_bf (_ . left) (_ () . right) ((x . xs) . code))
-  (_bf (_ . left) (_ () . right) code))
+(define-macro-rule (_)
+  (_bf (_ . left) (_ (_) . right) ((x . xs) . code))
+  (_bf (_ . left) (_ (_) . right) code))
 ;; end [
 
 (define (main)
